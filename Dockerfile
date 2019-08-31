9b8939927600e9d613fcc7f91064bc3b2f542841
Pawel Lbs <pawel.lbs@gmail.com>
2019-08-31 21:20:43 +0200

    x

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# Stage 0, "build-stage", based on Node.js, to build and compile Angular
+FROM node as build-stage
+
+WORKDIR /app
+COPY package*.json /app/
+RUN npm install
+COPY ./ /app/
+
+# We change output path to out, because angular cli normally uses projects name for the directory
+RUN npm run build -- --output-path=./dist/out
+
+# Stage 1, based on Nginx, to have only the compiled app, ready for production with Nginx
+FROM nginx:1.15
+COPY --from=build-stage /app/dist/out/ /usr/share/nginx/html
+COPY ./nginx.conf /etc/nginx/conf.d/default.conf
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { switchMap } from 'rxjs/operators';
+import { timer } from 'rxjs';
+
+const API = 'http://localhost:3000/';
+const API_MARK_AS_SEEN = API + 'mark';
+const API_NEW_ESTATE_SCRAPE = API + 'new';
+const DELAY = 60000 * 10;
+
+export interface Estate {
+  url: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'scraper2-frontend';
+export class AppComponent implements OnInit {
+  estates: Estate[] = [];
+  loading = false;
+  fetches = 0;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.getEstates();
+  }
+
+  markAsSeen(estateToMark: Estate): void {
+    console.log('marking as seen', estateToMark);
+    this.httpClient.post(API_MARK_AS_SEEN, estateToMark).subscribe();
+    this.estates = this.estates.filter((estate) => estateToMark !== estate);
+  }
+
+  async getEstates(): Promise<void> {
+    this.loading = true;
+    await Notification.requestPermission();
+
+    timer(0, DELAY).pipe(switchMap(() => {
+      return this.httpClient.get(API_NEW_ESTATE_SCRAPE);
+    })).subscribe((estates: Estate[]) => {
+      this.estates = estates;
+      this.loading = false;
+      this.fetches++;
+      this.notifyUser();
+    });
+  }
+
+  private notifyUser(): void {
+    const newEstatesLen = this.estates.length;
+    if (newEstatesLen) {
+      new Notification(`Found ${ newEstatesLen } new estates.`);
+    }
+  }
 }
