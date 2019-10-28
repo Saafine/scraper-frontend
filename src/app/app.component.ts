a3895b323e165e3fca897d013ec7ff9a7022c02d
Pawel Labus <pawel.labus@xtb.com>
2019-10-28 19:38:10 +0100

    X

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { QueryConfiguration, QuerySearchIgnoreDTO, QuerySearchResponse, ResultView, SearchQueryDTO, } from './app.model';
-import { switchMap } from 'rxjs/operators';
+import { catchError, switchMap } from 'rxjs/operators';
 import { NotificationService } from './services/notification.service';
-import { Subscription, timer } from 'rxjs';
+import { Subscription, throwError, timer } from 'rxjs';
 import { environment } from '../environments/environment';
+// @ts-ignore
+import QUERY_CONFIG from './query-config.json';
 
-export const API_WATCHER = `${environment.api}watch`;
-export const API_WATCHER_IGNORE = `${environment.api}watch/ignore`;
+export const API_WATCHER = `${ environment.api }watch`;
+export const API_WATCHER_IGNORE = `${ environment.api }watch/ignore`;
 const DELAY = 60000 * 10;
 
 // Array.from($('.single-result__content .single-result__title')).map((x) => x.textContent);
@@ -18,165 +20,14 @@ const DELAY = 60000 * 10;
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  configurations: QueryConfiguration[] = [
-    {
-      label: 'Dom i Porta do 500k, Ursynów',
-      url: 'https://www.domiporta.pl/mieszkanie/sprzedam/mazowieckie/ursynow?Price.To=500000&SortingOrder=InsertionDate',
-      items: [
-        {
-          label: 'description',
-          selector: '.sneakpeak__data .sneakpeak_description',
-          readMethod: 'textContent'
-        },
-        {
-          label: 'urls',
-          selector: '.sneakpeak__picture_container',
-          readMethod: 'href'
-        }
-      ]
-    },
-    {
-      label: 'Morizon do 500k, Ursynów',
-      url: 'https://www.morizon.pl/mieszkania/najnowsze/warszawa/ursynow/?ps%5Bprice_to%5D=500000',
-      items: [
-        {
-          label: 'titles',
-          selector: '.single-result__content .single-result__title',
-          readMethod: 'textContent'
-        },
-        {
-          label: 'description',
-          selector: '.single-result__content .description p',
-          readMethod: 'textContent'
-        },
-        {
-          label: 'urls',
-          selector: '.single-result__content a',
-          readMethod: 'href'
-        }
-      ]
-    },
-    {
-      label: 'Otodom do 500k, Ursynów',
-      url: 'https://www.otodom.pl/sprzedaz/mieszkanie/warszawa/ursynow/?search%5B' +
-        'filter_float_price%3Ato%5D=500000&search%5Bdescription%5D=1&search%5Bdistrict_id%5D=47&search%5Bcity_id%5D=26',
-      items: [
-        {
-          label: 'titles',
-          selector: 'article.offer-item .offer-item-details .offer-item-title',
-          readMethod: 'textContent'
-        },
-        {
-          label: 'urls',
-          selector: 'article.offer-item .offer-item-details h3 a',
-          readMethod: 'href'
-        }
-      ]
-    },
-    {
-      label: 'OLX do 500k, Ursynów',
-      url: 'https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/warszawa/' +
-        '?search%5Bfilter_float_price%3Ato%5D=500000&search%5Bdistrict_id%5D=373',
-      items: [
-        {
-          label: 'titles',
-          selector: '#offers_table .offer-wrapper h3',
-          readMethod: 'textContent'
-        },
-        {
-          label: 'urls',
-          selector: '#offers_table .offer-wrapper h3 a',
-          readMethod: 'href'
-        }
-      ]
-    },
-    {
-      label: 'Gumtree do 500k, Ursynów',
-      url: 'https://www.gumtree.pl/s-mieszkania-i-domy-sprzedam-i-kupie/ursynow/mieszkanie/v1c9073l3200020a1dwp1?pr=,500000',
-      items: [
-        {
-          label: 'titles',
-          selector: '.view .tile-title-text',
-          readMethod: 'textContent'
-        },
-        {
-          label: 'urls',
-          selector: '.view .tile-title-text',
-          readMethod: 'href'
-        }
-      ]
-    },
-    {
-      label: 'Otodom do 450k, all',
-      url: `https://www.otodom.pl/sprzedaz/mieszkanie/?search%5Bfilter_float_price%3Ato%5D=450000&search%5Bdescription
-      %5D=1&search%5Bprivate_business%5D=private&search%5Border%5D=created_at_first%3Adesc&locations%5B0%5D%5Bregion_id%5D=7&location
-      s%5B0%5D%5Bsubregion_id%5D=197&locations%5B0%5D%5Bcity_id%5D=26&locations%5B0%5D%5Bdistrict_id%5D=728&locations%5B0%5D%5Blocati
-      on_id%5D=7&locations%5B1%5D%5Bregion_id%5D=7&locations%5B1%5D%5Bsubregion_id%5D=197&locations%5B1%5D%5Bcity_id%5D=26&locations%
-      5B1%5D%5Bdistrict_id%5D=47&locations%5B1%5D%5Blocation_id%5D=7&locations%5B2%5D%5Bregion_id%5D=7&locations%5B2%5D%5Bsubregion_i
-      d%5D=197&locations%5B2%5D%5Bcity_id%5D=26&locations%5B2%5D%5Bdistrict_id%5D=117&locations%5B2%5D%5Blocation_id%5D=7&locations%5
-      B3%5D%5Bregion_id%5D=7&locations%5B3%5D%5Bsubregion_id%5D=197&locations%5B3%5D%5Bcity_id%5D=26&locations%5B3%5D%5Bdistrict_id%5
-      D=42&locations%5B3%5D%5Blocation_id%5D=7&locations%5B4%5D%5Bregion_id%5D=7&locations%5B4%5D%5Bsubregion_id%5D=197&locations%5B4%
-      5D%5Bcity_id%5D=26&locations%5B4%5D%5Bdistrict_id%5D=44&locations%5B4%5D%5Blocation_id%5D=7&locations%5B5%5D%5Bregion_id%5D=7&loc
-      ations%5B5%5D%5Bsubregion_id%5D=197&locations%5B5%5D%5Bcity_id%5D=26&locations%5B5%5D%5Bdistrict_id%5D=39&locations%5B5%5D%5Bloca
-      tion_id%5D=7&locations%5B6%5D%5Bregion_id%5D=7&locations%5B6%5D%5Bsubregion_id%5D=197&locations%5B6%5D%5Bcity_id%5D=26&locations
-      %5B6%5D%5Bdistrict_id%5D=300430&locations%5B6%5D%5Blocation_id%5D=7&locations%5B7%5D%5Bregion_id%5D=7&locations%5B7%5D%5Bsubreg
-      ion_id%5D=197&locations%5B7%5D%5Bcity_id%5D=26&locations%5B7%5D%5Bdistrict_id%5D=45&locations%5B7%5D%5Blocation_id%5D=7&location
-      s%5B8%5D%5Bregion_id%5D=7&locations%5B8%5D%5Bsubregion_id%5D=197&locations%5B8%5D%5Bcity_id%5D=26&locations%5B8%5D%5Bdistrict_id
-      %5D=300428&locations%5B8%5D%5Blocation_id%5D=7`,
-      items: [
-        {
-          label: 'titles',
-          selector: 'article.offer-item .offer-item-details .offer-item-title',
-          readMethod: 'textContent'
-        },
-        {
-          label: 'urls',
-          selector: 'article.offer-item .offer-item-details h3 a',
-          readMethod: 'href'
-        }
-      ]
-    },
-    {
-      label: 'OLX do 450k, all',
-      url: `https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/warszawa
-      /?search%5Bfilter_float_price%3Ato%5D=450000&search%5Bprivate_business%5D=private`,
-      items: [
-        {
-          label: 'titles',
-          selector: '#offers_table .offer-wrapper h3',
-          readMethod: 'textContent'
-        },
-        {
-          label: 'urls',
-          selector: '#offers_table .offer-wrapper h3 a',
-          readMethod: 'href'
-        }
-      ]
-    },
-    {
-      label: 'Gumtree do 450k, all',
-      url: 'https://www.gumtree.pl/s-mieszkania-i-domy-sprzedam-i-kupie/warszawa/mieszkanie/v1c9073l3200008a1dwp1?pr=,450000&df=ownr',
-      items: [
-        {
-          label: 'titles',
-          selector: '.view .tile-title-text',
-          readMethod: 'textContent'
-        },
-        {
-          label: 'urls',
-          selector: '.view .tile-title-text',
-          readMethod: 'href'
-        }
-      ]
-    }
-  ];
+  configurations: QueryConfiguration[] = QUERY_CONFIG;
 
+  errors = 0;
   loading = false;
   fetches = 0;
   results: ResultView[] = [];
   private sub: Subscription = new Subscription();
 
-  // TODO [P. Labus] split to service?
   constructor(private httpClient: HttpClient, private notificationService: NotificationService) {
   }
 
@@ -188,7 +39,11 @@ export class AppComponent implements OnInit, OnDestroy {
     this.initializeScraping();
     this.sub = timer(0, DELAY).pipe(switchMap(() => {
       return this.httpClient.post(API_WATCHER, this.getSearchQueryConfiguration(confNumber));
+    }), catchError((err) => {
+      this.errors++;
+      return throwError(err);
     })).subscribe((response: QuerySearchResponse) => {
+      this.fetches++;
       this.finishScraping();
       const result: ResultView = {
         queryHash: response.queryHash,
@@ -218,7 +73,6 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   private initializeScraping(): void {
-    this.fetches++;
     this.loading = true;
   }
 
